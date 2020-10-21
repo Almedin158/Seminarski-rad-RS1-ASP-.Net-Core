c5f1967095455c46ac0852b8d21211a90e3633b6
Almedin Alikadić <almedin.alikadic@edu.fit.ba>
2020-10-21 22:15:06 +0200

    Seminarski rad, radio skupa sa kolegom

---
+++ Controllers/OnamaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PekaraKonjic.Controllers
+{
+    public class OnamaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Proizvod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PekaraKonjic.Models
+{
+    public class Proizvod
+    {
+        public Proizvod()
+        {
+            Komentari = new List<Komentar>();
+            LikeBrojac = 0;
+        }        
+        public int ProizvodID { get; set; }
+        [Required]
+        public string Naziv { get; set; }
+        [Required]
+        public float Cijena { get; set; }
+        [DisplayName("Slika (Url)")]
+        [Required]
+        public string SlikaUrl { get; set; }
+        public int LikeBrojac { get; set; }
+        public string Opis { get; set; }
+        [DisplayName("Na stanju")]
+        [Required]
+        public bool NaStanju { get; set; }
+        public List<Komentar> Komentari { get; set; }
+        public int KategorijaId { get; set; }
+        public Kategorija Kategorija { get; set; }
+    }
+}
